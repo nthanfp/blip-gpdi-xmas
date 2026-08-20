dc28b77dfd2b3a2c02f5d86ea1ec67bc7bd289a8
Nathan <ntnferry1505@gmail.com>
2026-08-20 09:32:31 +0700

    FEAT: Update footer copyright year dynamically and replace various icon assets

--- application/views/partial/activities/footer.php
+++ application/views/partial/activities/footer.php
@@ -2,6 +2,6 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2026 <a href="https://internalgrup.id">PT Internal Tekstil Grup</a>.</strong> All rights
+    Copyright &copy; <?php echo date('Y'); ?> <strong><a href="https://internalgrup.id">PT Internal Tekstil Grup</a>.</strong> All rights
     reserved.
 </footer>
