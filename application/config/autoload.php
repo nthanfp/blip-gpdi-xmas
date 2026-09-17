24108dcb0b2b374be59342b16e4c7cca27129e90
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2026-09-17 20:22:15 +0700

    FEAT: Modify auto-loading of libraries to conditionally bypass database connection

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'form_validation');
+$autoload['libraries'] = file_exists(APPPATH . 'config/db_bypass.php') ? array('session', 'form_validation') : array('database', 'session', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
